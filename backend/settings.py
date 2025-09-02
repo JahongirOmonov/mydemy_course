dacef7aabd491e8c202fc30ccee380dfec19ad09
FirePlay <dodajoy0777@gmail.com>
2025-09-03 01:11:21 +0500

    r

--- backend/settings.py
+++ backend/settings.py
@@ -160,7 +160,7 @@ EMAIL_USE_SSL = False
 import os
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles" 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
